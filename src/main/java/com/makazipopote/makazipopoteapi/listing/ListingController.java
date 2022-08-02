d954e353a5397822a8ab1fa28e02411d605bcbd8
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-02 17:27:05 +0300

    initial commit

--- src/main/java/com/makazipopote/makazipopoteapi/listing/ListingController.java
+++ src/main/java/com/makazipopote/makazipopoteapi/listing/ListingController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
-//@CrossOrigin(origins = "https://makaziproperties.herokuapp.com")
+@CrossOrigin(origins = "https://makaziproperties.herokuapp.com")
 public class ListingController {
     private final ListingService listingService;
 
